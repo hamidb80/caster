7605024122e1c244a27e1fa849a48ecfe4617cb1
hamidb80 <hr.bolouri@gmail.com>
2023-07-13 15:19:37 +0430

    pass tests

--- src/caster.nim
+++ src/caster.nim
@@ -4,15 +4,15 @@ import macroplus
 macro caster*(def): untyped =
   ## a sugar for casting parameters in the procedure body,
   ## It eliminates the need for manually casting.
-  ## 
+  ##
   ## for an illustration, the 2 procedures are the same:
-  
+
   runnableExamples:
-    proc toNumericRepr1(c: char): string = 
+    proc toNumericRepr1(c: char): string =
       let b = cast[byte](c)
       $b
-    
-    proc toNumericRepr2(b: char as byte): string {.caster.} = 
+
+    proc toNumericRepr2(b: char as byte): string {.caster.} =
       $b
 
     assert "97" == toNumericRepr1 'a'
@@ -24,9 +24,9 @@ macro caster*(def): untyped =
 
   for i, p in def.params:
     if i != 0: # ignore return type
-      for id in p[IdentDefNames]:
-        let t = p[IdentDefType]
-        if t.matchInfix "as":
+      let t = p[IdentDefType]
+      if t.matchInfix "as":
+        for id in p[IdentDefNames]:
           before.add newLetStmt(id, newTree(nnkCast, t[InfixRightSide], id))
           result.params[i][IdentDefType] = t[InfixLeftSide]
 
--- tests/test.nim
+++ tests/test.nim
@@ -8,21 +8,39 @@ type
     mousePos: tuple[x, y: int]
 
 
-test "char -> byte":
-  func toByteRepr(b: char as byte): string {.caster.} =
-    $b
+suite "basic":
+  test "char -> byte":
+    func toByteRepr(b: char as byte): string {.caster.} =
+      $b
 
-  check "97" == toByteRepr 'a'
+    check "97" == toByteRepr 'a'
 
 
-test "Inheritance":
-  proc wrap(callback: proc(e: Event)) =
-    callback Event MouseEvent(mousePos: (1, 2))
+  test "Inheritance":
+    proc wrap(callback: proc(e: Event)) =
+      callback Event MouseEvent(mousePos: (1, 2))
 
-  var acc = 0
+    var acc = 0
 
-  wrap proc(me: Event as MouseEvent) {.caster.} =
-    acc.inc me.mousePos.x
-    acc.inc me.mousePos.y * 10
+    wrap proc(me: Event as MouseEvent) {.caster.} =
+      acc.inc me.mousePos.x
+      acc.inc me.mousePos.y * 10
 
-  check acc == 21
+    check acc == 21
+
+suite "advanced":
+  test "multi args":
+    proc p1(c: char as byte, a: char, p: int as pointer) {.caster.} =
+      check c is byte
+      check a is char
+      check p is pointer
+
+    p1 '1', '2', 3
+
+  test "grouped params":
+    proc p2(b1, b2: char as byte, c: char) {.caster.} =
+      check b1 is byte
+      check b2 is byte
+      check c is char
+
+    p2 '1', '2', '3'
